6f4589c12c84b34738f73915977393de3deed629
Uday Manchanda <uday.manchanda14@gmail.com>
2020-01-26 01:53:16 -0500

    insertion of rows into table is complete

--- main.go
+++ main.go
@@ -1,42 +1,110 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
 // Footprint is a footprint
 type Footprint struct {
-	ConstructionYear int
-	Bin              int
+	ConstructionYear string
+	Bin              string
 	Moddate          string
 	Stattype         string
-	ID               int
-	Heightroof       float32
-	ShapeArea        float32
-	ShapeLen         float32
+	ID               string
+	Heightroof       string
+	ShapeArea        string
+	ShapeLen         string
 	Geomsource       string
 }
 
-func connectToDB() {
-	var host = "postgres://mzyygnzbzeszgb:46be8a7d16940eed1b4da8bf1f6ac7ec9616e0b073668918c5274c2576119a93@ec2-52-203-98-126.compute-1.amazonaws.com:5432/dcqcboq15tcb1e"
+const (
+	username = "mzyygnzbzeszgb"
+	// username = "postgres"
+	password = "46be8a7d16940eed1b4da8bf1f6ac7ec9616e0b073668918c5274c2576119a93"
+	host     = "ec2-52-203-98-126.compute-1.amazonaws.com"
+	// host = "footprints.celg0gvjzujb.us-east-1.rds.amazonaws.com"
+	port   = "5432"
+	dbName = "dcqcboq15tcb1e"
+	// dbName = "footprints"
+)
 
-	db, err := sql.Open("postgres", host)
+func sqlFunctions() {
+	connStr := "postgres://" + username + ":" + password + "@" + host + ":" + port + "/" + dbName
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	createSQLStatement := `CREATE TABLE IF NOT EXISTS footprints (
+		ConstructionYear integer,
+		Bin integer,
+		Moddate date,
+		Stattype text,
+		Id integer,
+		Heightroof numeric,
+		ShapeArea numeric,
+		ShapeLen numeric,
+		Geomsource text
+	)`
+
+	_, err = db.Exec(createSQLStatement)
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	footprintsCSVFile, err := os.Open("building.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	reader := csv.NewReader(bufio.NewReader(footprintsCSVFile))
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println(err)
+		}
+
+		insertSQLStatement := `INSERT INTO footprints (ConstructionYear, Bin, Moddate, Stattype, Id, Heightroof, ShapeArea, ShapeLen, Geomsource)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+		RETURNING id`
+		id := 0
+
+		err = db.QueryRow(insertSQLStatement, line[0], line[1], line[4], line[5], line[6], line[7], line[10], line[11], line[14]).Scan(&id)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
 	defer db.Close()
+}
 
-	err = db.Ping()
+func selectData(db *sql.DB) *sql.Rows {
+	selectSQLStatement := `SELECT * FROM footprints LIMIT 50`
+	rows, err := db.Query(selectSQLStatement)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println("Successfully connected")
+	return rows
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
 }
 
 func main() {
-	connectToDB()
+	r := mux.NewRouter()
+	r.HandleFunc("/", index)
+	http.ListenAndServe(":8000", r)
 }
